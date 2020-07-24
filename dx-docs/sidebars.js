5cc0991648f6990d323b10f61747328e67b90461
Dani <dani.simeonov@stratusolve.com>
2020-07-24 11:00:21 +0200

    getting started done started common examples
    merged hello world example with common examples

--- dx-docs/sidebars.js
+++ dx-docs/sidebars.js
@@ -1,13 +1,20 @@
 module.exports = {
     someSidebar: {
-        Introduction: ['doc1', 'getting-started'],
-        'Getting Started': ['doc2'],
+        'Getting Started': [
+            'getting-started-ways-to-get-started',
+            'getting-started-1-sandbox',
+            'getting-started-2-virtualbox',
+            'getting-started-3-from-scratch',
+        ],
         'The Basics': [
             'the-basics-configuration',
             'the-basics-data-modeler',
             'the-basics-divblox-components',
             'the-basics-divblox-apis',
             'the-basics-native-support'],
-        Setup: ['mdx'],
+        'Common Examples': [
+            'common-examples-hello-world-example',
+            'common-examples-other-common-examples'
+        ]
     },
 };
